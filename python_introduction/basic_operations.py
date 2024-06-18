e5cf46182b96171aba4ef2a357a4324404d48da1
aeikun <119795798+aeikun@users.noreply.github.com>
2024-06-18 17:47:42 +0100

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,10 @@
-number1 = 5
-number2 = 10
+number1 = 10
+number2 = 5
 
-print(f"Addition result is: {number1 + number2}");
-print(f"Substraction result is: {number2 - number1}");
-print(f"Multiplication result is: {number1 * number2}");
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
