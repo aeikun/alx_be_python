074edfe51c4db2b9b4ed17961a0ba38ad31c3834
aeibo <aeibouyhy@gmail.com>
2024-06-18 17:42:11 +0100

    Basic Arithmetic python script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 5
+number2 = 10
+
+print(f"Addition result is: {number1 + number2}");
+print(f"Substraction result is: {number2 - number1}");
+print(f"Multiplication result is: {number1 * number2}");
