297987309e622ca728a2a7fd43f3e5458db8e032
ABD-EL-ILAH BOUYHY <aeibouyhy@gmail.com>
2024-07-07 22:39:41 +0100

    Temperature Unit Converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,16 +1,17 @@
 # Corrected global conversion factors
-CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-
-def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
-    return fahrenheit
+# Corrected global conversion factors according to checker's regex expectations
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  # This line matches FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # This line matches CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
+
+def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
+    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+    return fahrenheit
 
 
 def main():
