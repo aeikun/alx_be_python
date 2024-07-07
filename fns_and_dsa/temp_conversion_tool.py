c8f83ec161334758e03b9e01d04505b18c9d89bc
ABD-EL-ILAH BOUYHY <aeibouyhy@gmail.com>
2024-07-07 23:15:35 +0100

    Temperature Unit Converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -12,7 +12,7 @@ def convert_to_fahrenheit(celsius):
     return fahrenheit
 
 def main():
-    try:
+    
         temperature_str = input("Enter the temperature to convert: ")
         temperature = float(temperature_str)
         unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip()
@@ -28,10 +28,6 @@ def main():
 
         print(f"{temperature:.2f}°{unit.upper()} is {converted_temp:.2f}°{target_unit}")
 
-    except ValueError as ve:
-        print(f"Error: {ve}. Please enter a numeric value for temperature.")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     main()
