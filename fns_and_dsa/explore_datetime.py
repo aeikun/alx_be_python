4f7f3850f9b6b749baa8a6f13fc585836f1e0a17
ABD-EL-ILAH BOUYHY <aeibouyhy@gmail.com>
2024-07-07 21:54:05 +0100

    Explore Future

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    # Get the current date and time
+    current_datetime = datetime.now()
+
+    # Get the current date and time
+    current_date = current_datetime.date()
+
+    # Formatting the date and time
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_datetime}")
+
+def calculate_future_date():
+    # Prompt the user to enter a number of days
+    days_to_add = int(input("Enter the number of days to add to the current date: "))
+    
+    # Get the current date
+    current_date = datetime.now().date()
+    
+    # Calculate the future date by adding the specified number of days
+    future_date = current_date + timedelta(days=days_to_add)
+    
+    # Print the future date in a readable format
+    print(f"Future date: {future_date}")
+
+
+def main():
+    display_current_datetime()
+    calculate_future_date()
+
+
+if __name__ == "__main__":
+    main()
