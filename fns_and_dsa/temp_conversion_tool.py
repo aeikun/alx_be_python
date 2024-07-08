d22d9e165f0ae8840096938eca68d757f338c0f9
ABD-EL-ILAH BOUYHY <aeibouyhy@gmail.com>
2024-07-08 12:52:40 +0100

    Temperature Unit Converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -24,7 +24,7 @@ def main():
             converted_temp = convert_to_fahrenheit(temperature)
             print(f"{temperature}°C is {converted_temp}°F")
         else:
-            print("Invalid temperature unit. Please enter 'C' or 'F'.")
+            print("Invalid temperature. Please enter a numeric value.")
     
 
 if __name__ == "__main__":
